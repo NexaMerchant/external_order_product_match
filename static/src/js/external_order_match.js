6df3e1c220f32294ac75f8a732a3658f80bcdd9a
Steve <34465153+xxl4@users.noreply.github.com>
2025-05-15 18:20:34 +0800

    fix new black

--- static/src/js/external_order_match.js
+++ static/src/js/external_order_match.js
@@ -11,6 +11,8 @@ odoo.define('external_order_product_match.external_order_match', function (requi
         // 你的配对逻辑...
         // redirect to the match page with order_id and external_order_line_id as parameters
         var url = '/match_product?order_id=' + order_id + '&external_order_line_id=' + external_order_line_id;
-        window.location.href = url;
+        // new black window
+        window.open(url, '_blank');
+        //window.location.href = url;
     });
 });
